889870a3b02597654a2ace5903da1dba2c96bbb9
cltubigon <cltubigon@gmail.com>
2025-04-09 02:47:03 +0800

    added profile picture feature3

--- src/app/hooks/useUploadImage.ts
+++ src/app/hooks/useUploadImage.ts
@@ -2,8 +2,30 @@
 import { useState, useCallback, useTransition } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { createClient } from '../config/supabase/supabaseClient'
-import { generateBlurDataUrl } from '../components/dropzone/generateBlurDataUrl'
 import { Database } from '@/types/database.types'
+
+const generateBlurDataUrl = async (file: File): Promise<string> => {
+  return new Promise((resolve) => {
+    const img = new Image()
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+      if (!e.target?.result) return
+
+      img.src = e.target.result as string
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = 20
+        canvas.height = 20
+        const ctx = canvas.getContext('2d')
+        ctx?.drawImage(img, 0, 0, 20, 20)
+        resolve(canvas.toDataURL('image/jpeg', 0.6))
+      }
+    }
+
+    reader.readAsDataURL(file)
+  })
+}
 
 export type I_LocalImage = {
   file: File
