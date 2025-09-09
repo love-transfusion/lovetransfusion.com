e87ac806179c335984844bae7b26e84a303a0274
cltubigon <cltubigon@gmail.com>
2025-09-09 10:50:26 +0800

    before migration

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { selectandupdatefacebookposts } from './actions'
+import axios from 'axios'
 
 const TestPage = async () => {
-  const { data } = await selectandupdatefacebookposts()
-  return (
-    <>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </>
+  const { data } = await axios.post(
+    'http://localhost:3001/api/recipients',
+    { action: 'test', note: 'Hello from lt.com' },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'secret-key': process.env.LT_ORG_ROUTE_SECRET_KEY!,
+      },
+    }
   )
+  return <pre>{JSON.stringify(data, null, 2)}</pre>
 }
 
 export default TestPage
