c1de6f676c75fefd9e680a785d74014b491b0d77
cltubigon <cltubigon@gmail.com>
2025-09-11 17:45:19 +0800

    added auto revalidate

--- src/app/(logged-in-pages)/dashboard/[user_id]/@updateSlot/ClientUpdate.tsx
+++ src/app/(logged-in-pages)/dashboard/[user_id]/@updateSlot/ClientUpdate.tsx
@@ -1,9 +1,22 @@
 'use client'
 import useToolTipFetchLazilyOrInsertTooltips from '@/app/hooks/this-website-only/useToolTipFetchLazilyOrInsertTooltips'
-import React from 'react'
+import { util_customRevalidatePath } from '@/app/utilities/util_customRevalidatePath'
+import React, { useEffect } from 'react'
 
 const ClientUpdate = () => {
   useToolTipFetchLazilyOrInsertTooltips({ clPath: '/dashboard' })
+
+  const revalidate = async () => {
+    await util_customRevalidatePath('/')
+  }
+
+  useEffect(() => {
+    const interval = setInterval(revalidate, 2 * 60 * 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
   return <div></div>
 }
 
