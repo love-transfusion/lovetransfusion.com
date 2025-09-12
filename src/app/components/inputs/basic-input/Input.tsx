6d75aea167406e944ae37591dc3e64e508c141d8
cltubigon <cltubigon@gmail.com>
2025-09-12 12:50:54 +0800

    updated input component

--- src/app/components/inputs/basic-input/Input.tsx
+++ src/app/components/inputs/basic-input/Input.tsx
@@ -47,36 +47,38 @@ const Input = forwardRef<HTMLInputElement, I_Input>(function Input(
 ) {
   const variantStyles = getVariant(clVariant, clLeftIcon, clRightIcon)
   return (
-    <div className={twMerge('relative', clContainerClassName)}>
-      {clLeftIcon && (
-        <div
-          className={twMerge(
-            'absolute top-0 bottom-0 my-auto left-[6px] z-20 h-fit text-primary',
-            clIconClassName
-          )}
-        >
-          {clLeftIcon}
-        </div>
-      )}
-      {clRightIcon && (
-        <div
-          className={twMerge(
-            'absolute top-0 bottom-0 my-auto right-[6px] z-20 h-fit text-primary',
-            clIconClassName
-          )}
-        >
-          {clRightIcon}
-        </div>
-      )}
-      <input
-        ref={ref}
-        {...props}
-        value={clValue}
-        placeholder={clPlaceholder}
-        className={twMerge(``, variantStyles, className)}
-      />
+    <div className={twMerge('w-full', clContainerClassName)}>
+      <div className={'relative w-full'}>
+        {clLeftIcon && (
+          <div
+            className={twMerge(
+              'absolute top-0 bottom-0 my-auto left-[6px] z-20 h-fit text-primary',
+              clIconClassName
+            )}
+          >
+            {clLeftIcon}
+          </div>
+        )}
+        {clRightIcon && (
+          <div
+            className={twMerge(
+              'absolute top-0 bottom-0 my-auto right-[6px] z-20 h-fit text-primary',
+              clIconClassName
+            )}
+          >
+            {clRightIcon}
+          </div>
+        )}
+        <input
+          ref={ref}
+          {...props}
+          value={clValue}
+          placeholder={clPlaceholder}
+          className={twMerge(``, variantStyles, className)}
+        />
+      </div>
       {clErrorMessage && (
-        <p className={'text-sm text-red-500'}>{clErrorMessage}</p>
+        <p className={'text-sm text-red-500 mt-[2px]'}>{clErrorMessage}</p>
       )}
     </div>
   )
